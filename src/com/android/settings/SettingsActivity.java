5bd3f1c550b4514b98bb33d2bc106b53fa2df698
Kyrylo Mikos <kiril.mik.os@gmail.com>
2014-11-17 13:26:26 +0200

    Settings: Squashed commit for expanded desktop
    Settings: Bringup expanded desktop and implement per-user configuration.
    
    Change-Id: Ib673d278d23423fe6885c73f1eb3dd858633e5b6
    
    Settings: Don't load per-app extdesk into memory if not USER_CONFIGURABLE.
    
    Change-Id: Iae4857e0cd95d75ae29222bb45a8fdcf0ff1ec48
    
    Settings: Only save settings on destroy if we're user configurable.
    
    Change-Id: Ie459053b4614f818dfa19948709a3dd0fdb8f427
    
    Settings: Fix off-by-one SectionIndexer in Expanded Desktop.
    
    Change-Id: I7bdd1081114b40cc2ab6372d16bb247401f83a86
    
    Settings: Create default expanded desktop style fragment.
    
    Change-Id: Ia5bebb45a953c2a8efdffbab2ddcbfa3d3c3d486
    
    Break out status bar icon from extdesk icon.
    
    Change-Id: I54165bd4338bb1d73f1d453f75ee1c06cfd62a8f
    
    Settings: Make sure to rebuild on app entries loaded.
    
    Change-Id: I61572ff082629046d1fdf5fedb1e8ec37343f588

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -60,6 +60,7 @@ import com.android.settings.accounts.ChooseAccountActivity;
 import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AdvancedAppSettings;
 import com.android.settings.applications.DrawOverlayDetails;
+import com.android.settings.applications.ExpandedDesktopPreferenceFragment;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ManageAssist;
@@ -359,6 +360,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             PixNSettings.class.getName(),
             AmbientSettings.class.getName()
             DisplayRotation.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName()
     };
 
 
