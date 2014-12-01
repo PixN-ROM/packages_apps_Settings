c2a0b1be7c6e82ad3516ce523333f05d87452b80
Muhammed Nazim <mnm9994u@gmail.com>
2014-12-01 01:04:20 -0700

    Add RAM info (2/2)
    Adapted for Nougat, moved getDeviceMemoryInfo into base
    
    Did not include CPU info because we already have it.
    
    KreAch3R: changes here and there to the original commit

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -60,6 +60,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_DEVICE_MODEL = "device_model";
     private static final String KEY_DEVICE_NAME = "device_name";
     private static final String KEY_DEVICE_PROCESSOR = "device_processor";
+    private static final String KEY_DEVICE_MEMORY = "device_memory";
     private static final String KEY_SELINUX_STATUS = "selinux_status";
     private static final String KEY_BASEBAND_VERSION = "baseband_version";
     private static final String KEY_FIRMWARE_VERSION = "firmware_version";
@@ -126,6 +127,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setValueSummary(KEY_BASEBAND_VERSION, "gsm.version.baseband");
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL + DeviceInfoUtils.getMsvSuffix());
         setStringSummary(KEY_DEVICE_PROCESSOR, DeviceInfoUtils.getDeviceProcessorInfo(getActivity()));
+        setStringSummary(KEY_DEVICE_MEMORY, DeviceInfoUtils.getDeviceMemoryInfo());
         setValueSummary(KEY_EQUIPMENT_ID, PROPERTY_EQUIPMENT_ID);
         setStringSummary(KEY_DEVICE_NAME, Build.PRODUCT);
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
